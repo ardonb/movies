eb4958193d8a047c84afbd909021ab013182901e
ardonb <ardon.bela@gmail.com>
2023-01-25 18:52:54 +0100

    add basic crud for reviews

---
+++ src/main/java/com/ardonb/movies/service/ReviewService.java
@@ -0,0 +1,33 @@
+package com.ardonb.movies.service;
+
+import com.ardonb.movies.model.Movie;
+import com.ardonb.movies.model.Review;
+import com.ardonb.movies.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository repository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = repository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviews").value(review))
+                .first();
+
+        return review;
+    }
+}
