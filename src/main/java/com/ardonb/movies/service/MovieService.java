f89715af75c8050f72eaf08d98e54a11682796c7
ardonb <ardon.bela@gmail.com>
2023-01-25 18:36:05 +0100

    add basic crud operations for movie

---
+++ src/main/java/com/ardonb/movies/service/MovieService.java
@@ -0,0 +1,26 @@
+package com.ardonb.movies.service;
+
+import com.ardonb.movies.model.Movie;
+import com.ardonb.movies.repository.MovieRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+
+
+}
